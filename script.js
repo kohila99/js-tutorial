1db2cc2f34ac09ccae69f09e1b097edc22049d68
Kohila A <ykohila@gmail.com>
2020-10-28 07:22:00 +0530

    data types and logical operators

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-alert('Alert box');
-console.log ('A message in console');
+var age=40
+console. log(age);
+age='I am neither young nor old';
+console.log(typeof age);
+var age=40
+console.log(typeof age);
+var result =10+20;
+console.log(result);
+result=result -5;
+console.log(result);
+result=result *2;
+console.log(result);
+result=result /5;
+console.log(result);
+result=result %3;
+console.log(result);
+result=result +1;
+console.log(result);
+result++;
+console.log(result);
+result--;
+console.log(result);
+++result;
+console.log(result);
+--result;
+console.log(result);
+var otherVar =++result + 5;
+console.log(otherVar);
+var lang = "Javascript";
+console.log("Hello, " + lang + "!");
+console.log(lang+101);
+console.log(2019-"sometext");
+
+
